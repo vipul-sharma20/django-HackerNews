ad60805e675dd457665178e969962566fb723ed2
vipul <vipul.sharma20@gmail.com>
2015-09-06 05:17:25 +0530

    code cleanup of views

--- contacts/views.py
+++ contacts/views.py
@@ -20,8 +20,6 @@ import operator
 
 class UserProfileDetailView(DetailView):
     """User Profile in detail view"""
-
-    #queryset = User.objects.filter(sel)
     slug_field = "username"
     template_name= "user_detail.html"
     model = get_user_model()
@@ -37,11 +35,6 @@ def landing(request):
     name = 'Foo Bar'
     context = RequestContext(request)
     t = get_template('landing.html')
-    """if request.user.is_authenticated():
-        #html = t.render(Context({'name': request.user.username}))
-        return HttpResponseRedirect('/accounts/articles')
-    else:
-        return render_to_response('landing.html')"""
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -87,8 +80,6 @@ def loggedin(request, personal_id=1):
     try:
         return render_to_response('articles.html',
                                     {'full_name': request.user.username,
-                                    #'location': UserProfile.objects.filter \
-                                    #(user=request.user).location,
                                     'reputation':UserProfile.objects.filter(user=request.user).reputation,})
     except:
         return render_to_response('articles.html',
@@ -133,16 +124,6 @@ def personal_info(request):
          form = PersonalForm()
     return render_to_response('personal_form.html', {'form': form}, context)
 
-"""def user_profile_edit(request):
-
-    model = UserProfile
-    template_name = 'user_edit.html'
-    form_class = UserForm
-
-    if request.method == 'POST':
-        return HttpResponseRedirect('/accounts/loggedin')
-    else:
-        return UserProfile.objects.get_or_create(user=request.user)[0]"""
 class UserProfileEditView(UpdateView):
 
     model = UserProfile
@@ -224,7 +205,6 @@ def like_article(request, article_id):
     if created:
         b = UserProfile.objects.get(user=rep_user)
         b.reputation += 10
-        #print b.reputation
         b.save()
     a.save()
     return HttpResponse(a.votes)
@@ -272,16 +252,13 @@ def newscontent(request, news_id):
 
     template = "content.html"
     content = NewsContent.objects.get(id=news_id)
-    #print dir(content)
     return HttpResponse(content.content)
 
 def get_submissions(request, slug):
 
-    print 'something something'
     template = 'submission.html'
     user = User.objects.get(username=str(slug))
     content = Articles.objects.filter(uploader=user)
-    print content
     return render_to_response(template, {'articles':content, \
                     'full_name':request.user.username}, RequestContext(request))
 
